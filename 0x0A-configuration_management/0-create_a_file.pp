fb436bf0513e62ac541f6c4ff5fb76bf885ea729
Amanuel Adamu <amanueladamu18@gmail.com>
2022-03-13 19:24:49 +0000

    adding mamdatory task

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp called school
+file { 'school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
