15f2346e5cd51068c98ce4877eaf16445aff33cd
Amanuel Adamu <amanueladamu18@gmail.com>
2022-03-21 20:54:54 +0000

    fix bugs

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,22 @@
 exec { 'apt-update':
-  command => '/usr/bin/apt update',
+  command  => 'sudo apt-get update',
+  provider => shell,
 }
 
 package { 'nginx':
     ensure  => present,
-    require => Exec['apt-update'],
 }
 
 file_line {'header':
-    ensure  => 'present',
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'listen 80 default_server;',
-    line    => 'add_header X-Served-By $hostname;',
-    require => Package['nginx'],
+    ensure => present,
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'listen 80 default_server;',
+    line   => "   location / {
+    add_header X-Served-By ${hostname};",
+    match  => '^\tlocation / {',
 }
 
 service { 'nginx':
-    ensure  => running,
-    require => Package['nginx'],
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
